40b950de00f661959442435f21e1360f323654f7
IslamAhmedAbuGhoneima <137311017+IslamAhmedAbuGhoneima@users.noreply.github.com>
2024-01-24 14:32:52 +0200

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -36,6 +36,21 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class SingupSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError("Password fields didn't match.")
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
